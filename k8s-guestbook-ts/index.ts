379ff17dd7a28d4daf3ae37777499c283fa5ee38
MitchellGerdisch <mitch@pulumi.com>
2024-07-21 22:19:28 -0500

    guestbook-ts use mlc

--- k8s-guestbook-ts/index.ts
+++ k8s-guestbook-ts/index.ts
@@ -1,8 +1,9 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as k8s from "@pulumi/kubernetes";
-import { StackSettings } from "@pequod/stackmgmt";
 
-import { ServiceDeployment } from "./servicedeployment";
+import { StackSettings } from "@pequod/stackmgmt";
+import { ServiceDeployment } from "@pequod/k8sapp";
+
 import { baseName, driftManagement, kubeconfig, pulumiAccessToken } from "./config";
 
 const k8sProvider = new k8s.Provider('k8s-provider', {
@@ -16,20 +17,20 @@ const guestbookNsName = guestbookNamespace.metadata.name
 const redisLeader = new ServiceDeployment("redis-leader", {
     image: "redis",
     namespace: guestbookNsName,
-    ports: [6379],
+    ports: 6379,
 }, { provider: k8sProvider });
 
 const redisReplica = new ServiceDeployment("redis-replica", {
     image: "pulumi/guestbook-redis-replica",
     namespace: guestbookNsName,
-    ports: [6379],
+    ports: 6379,
 }, { provider: k8sProvider });
 
 const frontend = new ServiceDeployment("frontend", {
     replicas: 3,
     image: "pulumi/guestbook-php-redis",
     namespace: guestbookNsName,
-    ports: [80],
+    ports: 80,
     allocateIpAddress: true,
 }, { provider: k8sProvider });
 
