9dba75a9117b8397d7cc36706ed7a39347d9cbf0
MitchellGerdisch <mitch@pulumi.com>
2024-07-30 14:53:22 -0500

    Modify k8s app templates to use environment instead of stack ref

--- k8s-container-ts/config.ts
+++ k8s-container-ts/config.ts
@@ -1,16 +1,27 @@
-import { Config, getOrganization, getProject, getStack, StackReference } from "@pulumi/pulumi";
+import { Config, getOrganization, getProject, getStack, Output, StackReference } from "@pulumi/pulumi";
 
 const config = new Config()
 
 export const baseName = config.get("baseName") || `${getProject()}-${getStack()}`
 export const driftManagement = config.get("driftManagement") || "Correct"
 
-// Get stack name of the base k8s infra to deploy to and get the kubeconfig for the cluster.
-const baseInfraStackName = config.get("baseInfraStackName") || "shared-dev-eks/dev"
-const k8sStackName = `${getOrganization()}/${baseInfraStackName}`
-const k8sStackRef = new StackReference(k8sStackName)
+// The default set up for this template is to leverage the shared-k8s-cluster ESC environment for the kubeconfig.
+// However, if the kubeconfig is not found, then use the stack reference to get the kubeconfig.
+// It's not necessarily a real-world use-case, but provides a way to contrast ESC-based stack references and in-code stack references.
+// The big talking point here is using ESC means this project does not need to be aware of the k8s cluster's stack.
+// It just gets it as config. 
+// This also enable testing use-cases since one can initialize a test stack and hand-copy a kubeconfig to use in the stack config file
+export var kubeconfig: Output<string> | Output<any> | undefined
+kubeconfig = config.getSecret("kubeconfig") 
 
-export const kubeconfig = k8sStackRef.requireOutput("kubeconfig") 
+// If no kubeconfig found in config (via ESC or otherwise), then use the stack reference to get the kubeconfig.
+if (!kubeconfig) {
+  // Get stack name of the base k8s infra to deploy to and get the kubeconfig for the cluster.
+  const baseInfraStackName = config.get("baseInfraStackName") || "shared-dev-eks/dev"
+  const k8sStackName = `${getOrganization()}/${baseInfraStackName}`
+  const k8sStackRef = new StackReference(k8sStackName)
+  kubeconfig = k8sStackRef.requireOutput("kubeconfig")
+} 
 
 const pulumiServiceConfig = new Config("pulumiservice")
 export const pulumiAccessToken = pulumiServiceConfig.getSecret("accessToken")
