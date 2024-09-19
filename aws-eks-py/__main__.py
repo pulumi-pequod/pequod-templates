478a7a7e0c40e9761c6f08d3a60970a92cbc80d9
MitchellGerdisch <mitch@pulumi.com>
2024-09-19 13:18:06 -0500

    eks-py match eks-ts

--- aws-eks-py/__main__.py
+++ aws-eks-py/__main__.py
@@ -1,68 +1,58 @@
-# Pulumi SDKs
+# Pulumi packages
 import pulumi
-from pulumi_aws import eks
+import pulumi_eks as eks
+import pulumi_awsx as awsx
 import pulumi_kubernetes as k8s
 
-# components
+# pequod packages
 from pequod_stackmgmt import StackSettings, StackSettingsArgs
 from pequod_k8sdatadog import K8sMonitor, K8sMonitorArgs
-from aws_network import Vpc, VpcArgs 
-
-# local python modules
-import iam
-import utils
 
 # Get stack-specific config
 config = pulumi.Config()
-service_name = config.get("service_name") or pulumi.get_project()
+base_name = config.get("service_name") or pulumi.get_project()
 desired_size = config.get_int("desiredClusterSize") or 2
 max_size = config.get_int("maxClusterSize") or 2
 min_size = config.get_int("minClusterSize") or 1
+vpc_network_cidr = config.get("network_cidr") or "10.0.0.0/16"
+eks_node_instance_type = config.get("node_instance_type") or "t2.medium"
 
-## VPC and related resources
-vpc = Vpc(f'{service_name}-net', VpcArgs()) 
+# Create a new VPC
+eks_vpc = awsx.ec2.Vpc(f"{base_name}-vpc",
+                       enable_dns_hostnames=True,
+                       cidr_block=vpc_network_cidr,
+                       nat_gateways=awsx.ec2.NatGatewayConfigurationArgs(
+                           strategy=awsx.ec2.NatGatewayStrategy.SINGLE
+                       ))
 
-## EKS Cluster
-eks_cluster = eks.Cluster(
-    f'{service_name}-cluster',
-    role_arn=iam.eks_role.arn,
-    tags={
-        'Name': 'pulumi-eks-cluster',
-    },
-    vpc_config=eks.ClusterVpcConfigArgs(
-        public_access_cidrs=['0.0.0.0/0'],
-        security_group_ids=[vpc.fe_security_group.id],
-        subnet_ids=vpc.subnet_ids,
-    ),
-    opts=pulumi.ResourceOptions(depends_on=[iam.eks_service_pol_attach, iam.eks_cluster_pol_attach, vpc])
-)
+# Create the EKS cluster
+eks_cluster = eks.Cluster(f"{base_name}-eks",
+                          vpc_id=eks_vpc.vpc_id,
+                          public_subnet_ids=eks_vpc.public_subnet_ids,
+                          private_subnet_ids=eks_vpc.private_subnet_ids,
+                          instance_type=eks_node_instance_type,
+                          desired_capacity=desired_size,
+                          min_size=min_size,
+                          max_size=max_size,
+                          node_associate_public_ip_address=False,
+                          endpoint_private_access=False,
+                          endpoint_public_access=True)
 
-eks_node_group = eks.NodeGroup(
-    f'{service_name}-nodegroup',
-    cluster_name=eks_cluster.name,
-    node_group_name='pulumi-eks-nodegroup',
-    node_role_arn=iam.ec2_role.arn,
-    subnet_ids=vpc.subnet_ids,
-    tags={
-        'Name': 'pulumi-cluster-nodeGroup',
-    },
-    scaling_config=eks.NodeGroupScalingConfigArgs(
-        desired_size=desired_size,
-        max_size=max_size,
-        min_size=min_size,
-    ),
-)
 
-kubeconfig = pulumi.Output.secret(utils.generate_kube_config(eks_cluster))
+# Instantiate a Kubernetes provider
+kubeconfig = pulumi.Output.secret(eks_cluster.kubeconfig)
+k8sprovider = k8s.Provider(f"{base_name}-k8sprovider", kubeconfig=kubeconfig)
 
-k8s_provider = k8s.Provider('k8s-provider', kubeconfig=kubeconfig, delete_unreachable=True)
-
-datadog_k8s_agent = K8sMonitor(f"{service_name}-mon", 
+# Deploy the Datadog agent
+datadog_k8s_agent = K8sMonitor(f"{base_name}-mon", 
     api_key=config.require_secret("datadogApiKey"),
-    opts=pulumi.ResourceOptions(provider=k8s_provider))
+    opts=pulumi.ResourceOptions(provider=k8sprovider))
 
-stackmgmt = StackSettings(f"{service_name}-stacksettings", 
+# Manage stack settings
+stackmgmt = StackSettings(f"{base_name}-stacksettings", 
                           drift_management=config.get("driftManagement"))
 
+# Export some useful information
 pulumi.export('kubeconfig', kubeconfig)
-pulumi.export('datadogDashboard', eks_cluster.name.apply(lambda name: f"https://app.datadoghq.com/dash/integration/86/kubernetes---overview?refresh_mode=sliding&tpl_var_cluster%5B0%5D={name}&live=true".lower()))
+pulumi.export('datadogDashboard', eks_cluster.eks_cluster.name.apply(lambda name: f"https://app.datadoghq.com/dash/integration/86/kubernetes---overview?fromUser=false&refresh_mode=sliding&tpl_var_cluster%5B0%5D={name}&live=true".lower()))
+                                                                                                                                                                # &tpl_var_cluster%5B0%5D=mitch-eks-ts-pequod-eks-ekscluster-ee942dc&from_ts=1726765011855&to_ts=1726765911855&live=true
