796ad96f9d956db9cbf5a222d04c5aa28a1c7d47
MitchellGerdisch <mitch@pulumi.com>
2025-03-11 11:38:33 -0500

    container-services-py tweak

--- container-services-py/__main__.py
+++ container-services-py/__main__.py
@@ -5,12 +5,20 @@ from pulumi_aws import s3
 
 from pulumi_container_services import AppDeploy, AppDeployArgs, AppImage, AppImageArgs 
 
-image = AppImage('my-image', 
-  docker_file_path="./app"
-)
+config = pulumi.Config()
+cpu = config.get_int("cpu") or 256 
+memory = config.get_int("memory") or 512
 
-# Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+app_image = AppImage(f"{pulumi.get_project()}-{pulumi.get_stack()}-image", AppImageArgs(
+  docker_file_path="./app"
+))
+
+app_service = AppDeploy(f"{pulumi.get_project()}-{pulumi.get_stack()}-app", AppDeployArgs(
+  image_reference=app_image.image_ref,
+  cpu=cpu,
+  memory=memory
+))
 
 # Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+pulumi.export("image_reference", app_image.image_ref)
+pulumi.export("service_url", pulumi.Output.concat("http://",app_service.loadbalancer_dns_name))
